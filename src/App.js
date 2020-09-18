a9bdb2ccfe088acbe30e40f8cbcf19dba497a5d1
Lior Rabinovich <lior.px@gmail.com>
2020-09-18 20:21:27 +0300

    Added not found route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Intro from './routes/Intro';
 import Chat from './routes/Chat';
 import Login from './routes/Login';
+import NotFound from './routes/NotFound'
 import AuthRoute from './components/AuthRoute'
 import GuestRoute from './components/GuestRoute'
 
 function App() {
   return (
     <Router>
-      <AuthRoute path="/" exact component={Intro} />
-      <GuestRoute path="/login" component={Login} />
-      <AuthRoute path="/chat/:chatId" component={Chat} />
-      <Route component={NotFound} />
+      <Switch>
+        <AuthRoute path="/" component={Intro} />
+        <GuestRoute path="/login" component={Login} />
+        <AuthRoute path="/chat/:chatId" component={Chat} />
+        <Route component={NotFound} />
+      </Switch>
     </Router>
   );
 }
